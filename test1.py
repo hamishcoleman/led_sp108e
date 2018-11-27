30faa5ee05b9603eaaad2ae9436e0576f648fbda
Hamish Coleman <hamish@zot.org>
2018-11-27 12:58:09 +0800

    Add remainder of initial conversation

--- test1.py
+++ test1.py
@@ -6,9 +6,7 @@
 import argparse
 
 import socket
-import codecs
-
-encode_hex = codecs.getencoder("hex_codec")
+import binascii
 
 def do_options():
     a = argparse.ArgumentParser('Reverse Engineer Protocol for SP108E')
@@ -17,15 +15,38 @@ def do_options():
 
     return a.parse_args()
 
+def txn_sync(sock, sendbytes):
+    """ Perform a synchronous transaction """
+
+
+    # TODO - if verbose
+    sendhex = binascii.hexlify(sendbytes).decode('utf-8')
+    print("> {}".format(sendhex))
+
+    sock.send(sendbytes)
+    recvbytes = sock.recv(4096)
+
+    # TODO - if verbose
+    recvhex = binascii.hexlify(recvbytes).decode('utf-8')
+    print("< {}".format(recvhex))
+
+    return recvbytes
+
+def txn_sync_expect(sock, sendbytes, expectbytes):
+    """ Perform a txn_sync() and confirm the result is as expected """
+    r = txn_sync(sock, sendbytes)
+    assert(r == expectbytes)
+    return r
+
 def main(args):
     print("Connecting to {}:{}".format(args.host, args.port))
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((args.host, args.port))
 
-    s.send(bytes.fromhex('38e523d3d583'))
-    buf = s.recv(512)
-    print("{}".format(encode_hex(buf)))
+    txn_sync_expect(s, b'8\xe5\x23\xd3\xd5\x83', b'\x01\x02\x03\x04\x05\xcf')
+    txn_sync_expect(s, b'8\xc5\x1b\xa9\xd5\x83', b'\x01\x02\x03\x04\x05\x6d')
+    txn_sync_expect(s, b'8\xd9\x0f\xbd\x10\x83', b'\x38\x01\xfc\x80\xff\x02\x00\x32\x00\x01\xff\x00\x00\x03\x00\xff\x83')
 
 if __name__ == '__main__':
     args = do_options()
